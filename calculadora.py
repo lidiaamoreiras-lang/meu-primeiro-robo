2414d432994eb7433fed22be80e0e50988f83549
Lídia Moreira <lidiaamoreira.s@gmail.com>
2026-01-06 19:00:08 +0000

    Calcularoda pronta e finalizada

--- calculadora.py
+++ calculadora.py
@@ -1,14 +1,17 @@
+from pyscript import Element
+
 def calcular(operacao):
-    n1 = float(elemento_n1.value)
-    n2 = float(elemento_n2.value)
+    # Use .value diretamente, é mais seguro nesta versão
+    n1_val = Element("n1").value
+    n2_val = Element("n2").value
     
-    if operacao == '+':
-        resultado = n1 + n2
-    elif operacao == '-':
-        resultado = n1 - n2
-    elif operacao == '*':
-        resultado = n1 * n2
-    elif operacao == '/':
-        resultado = n1 / n2
+    if not n1_val or not n2_val:
+        Element("resultado").element.innerText = "Digite os números!"
+        return
+
+    num1 = float(n1_val)
+    num2 = float(n2_val)
     
-    elemento_resultado.innerHTML = f"Resultado: {resultado}"
+    # ... resto do seu código de soma, subtração, etc ...
+    
+    Element("resultado").element.innerText = f"Resultado: {res}"
