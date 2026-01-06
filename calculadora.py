3c38e92ec2af33a0354c28981e6738ba52a117c9
Lídia Moreira <lidiaamoreira.s@gmail.com>
2026-01-06 19:31:57 +0000

    Correcao final da variavel res

--- calculadora.py
+++ calculadora.py
@@ -1,17 +1,33 @@
 from pyscript import Element
 
 def calcular(operacao):
-    # Use .value diretamente, é mais seguro nesta versão
-    n1_val = Element("n1").value
-    n2_val = Element("n2").value
+    # Esta linha garante que 'res' sempre exista
+    res = 0 
     
-    if not n1_val or not n2_val:
-        Element("resultado").element.innerText = "Digite os números!"
-        return
+    try:
+        # Pega os valores das caixas usando os IDs exatos
+        n1_val = Element("n1").value
+        n2_val = Element("n2").value
+        
+        if not n1_val or not n2_val:
+            Element("resultado").element.innerText = "Resultado: Digite os números!"
+            return
 
-    num1 = float(n1_val)
-    num2 = float(n2_val)
-    
-    # ... resto do seu código de soma, subtração, etc ...
-    
-    Element("resultado").element.innerText = f"Resultado: {res}"
+        num1 = float(n1_val)
+        num2 = float(n2_val)
+        
+        # Realiza a operação
+        if operacao == '+': 
+            res = num1 + num2
+        elif operacao == '-': 
+            res = num1 - num2
+        elif operacao == '*': 
+            res = num1 * num2
+        elif operacao == '/': 
+            res = num1 / num2 if num2 != 0 else "Erro: Divisão por 0"
+            
+        # Mostra o resultado final
+        Element("resultado").element.innerText = f"Resultado: {res}"
+        
+    except Exception as e:
+        Element("resultado").element.innerText = "Erro: Use apenas números!"
