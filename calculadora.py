407b40d3636156c4ea702a55f3f924afcd8e65ba
Lídia Moreira <lidiaamoreira.s@gmail.com>
2026-01-06 17:18:37 +0000

    Calculadora PRONTA

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+print("--- CALCULADORA MÁGICA EM PYTHON ---")
+nome = input("Qual é o seu nome? ")
+print(f"Olá {nome}! Vamos somar?")
+
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+
+resultado = num1 + num2
+print(f"O robô calculou que a soma é: {resultado}")
